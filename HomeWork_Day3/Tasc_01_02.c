6e5f7b59c24bafa87b5b4875e5875fb64aebb157
codemenivo <codemen@abv.bg>
2022-08-26 16:21:52 +0300

    Add files via upload

--- HomeWork_Day3/Tasc_01_02.c
+++ HomeWork_Day3/Tasc_01_02.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
+
 double calculateBmi(double index, double mass, double powerP) {
     return index * mass / powerP;
 }
@@ -32,3 +33,24 @@ int main(int argc, char const *argv[])
 
     return 0;
 }
+
+/* The math functions in math.h have implementations in libm.so 
+(or libm.a for static linking), and libm is not linked in by default. 
+There are historical reasons for this libm/libc split, none of them 
+very convincing.
+
+This solved my issue. Nevertheless, I have 2 Ubuntu 20.04 boxes
+ with Code::Blocks installed as IDE. One compiles the source code  
+ (including math functions) properly without -lm, and the other needs -lm.
+
+
+Compiling: 
+ivo@ivo-ASUS-TUF-Gaming-F17-FX706HCB-FX706HCB:
+~/HomeWorks/HomeWork_Day3$ gcc Tasc_01_02.c -o Tasc_01_02 -lm
+
+Displaying:
+ivo@ivo-ASUS-TUF-Gaming-F17-FX706HCB-FX706HCB:
+~/HomeWorks/HomeWork_Day3$ ./Tasc_01_02
+
+
+*/
