eb09600294b54b2fde0ff34cd0e7cf1859fed8a2
codemenivo <codemen@abv.bg>
2022-08-26 19:46:26 +0300

    Add files via upload

--- HomeWork_Day3/Tasc_02_02.c
+++ HomeWork_Day3/Tasc_02_02.c
@@ -1,17 +1,13 @@
 #include<stdio.h>
 #include<math.h>
 
-double areaHorizontalCilinder(double radiusR, double heightH)
-{
-    return ((1 / (cos((((radiusR - heightH) / (radiusR)) * pow(radiusR, 2.0))))) - 
-            ((radiusR - heightH) * (sqrt((2 * radiusR * heightH)-(pow(heightH, 2.0))))));            
-            
-}
+double calculateAreaHorizCilinder(double radiusR, double heightH);
+
 
 int main(int argc, char const *argv[])
 {
     /* Horizontal Cilinder */ 
-    /* Area = ((1 /cos ((r - h) / r) * r^2)) - ((r - h) * (sqrt((2 * r * h) - h^2)))*/
+    /* Area = ((cos^-1 ((r - h) / r) * r^2)) - ((r - h) * (sqrt((2 * r * h) - h^2)))*/
     /* Volume = Area * Length  */
 
     double radiusR, lengthL, heightH, volumeV, areaA;
@@ -22,7 +18,7 @@ int main(int argc, char const *argv[])
     printf("Please enter HeightH in meters: ");
     scanf("%lf", &heightH);
 
-    areaA = areaHorizontalCilinder(radiusR, heightH);
+    areaA = calculateAreaHorizCilinder(radiusR, heightH);
     volumeV = areaA * lengthL;
 
     printf("Area in sqare meters is: %.3lf\n", areaA);
@@ -32,9 +28,18 @@ int main(int argc, char const *argv[])
 
     return 0;
 }
+
+double calculateAreaHorizCilinder(double radiusR, double heightH)
+{
+    return ((acos((radiusR - heightH) / (radiusR)) * pow(radiusR, 2.0)) - 
+            ((radiusR - heightH) * (sqrt((2 * radiusR * heightH)-(pow(heightH, 2.0))))));            
+            
+}
+
 /*
 ivo@ivo-ASUS-TUF-Gaming-F17-FX706HCB-FX706HCB:~/HomeWork_Day3$ gcc Tasc_02_02.c -o Tasc_02_02 -lm
 ivo@ivo-ASUS-TUF-Gaming-F17-FX706HCB-FX706HCB:~/HomeWork_Day3$ ./Tasc_02_02
 Poluchavam otricatelni rezultati, koeto ne e viarno!
 Da nameria Greshkata!
+cos^-1 is an acos function in C language.
 */
