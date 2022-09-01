0fdb6b81a202eacd0f1d3c3a5555d37489a2f0bf
codemenivo <codemen@abv.bg>
2022-09-01 13:17:16 +0300

    Add files via upload

--- HW_4/Task_3_StrFunc.c
+++ HW_4/Task_3_StrFunc.c
@@ -4,13 +4,29 @@
 int main()
 {
   char str1[100] = "Program language C";
-
+  char str2[50];  
+  char str3[50] = " course";
   
 
 
 printf("Length of the strig is: %ld\n", strlen(str1));
 printf("StrnLen return max size is: %ld\n", strnlen(str1, 20));
 printf("StrnLen return specified size is: %ld\n", strnlen(str1, 5));
+
+strcpy(str2, str1);
+printf("Content of second string is: %s\n", str2);
+memset(str2, 0, sizeof(str2));
+strncpy(str2, str1, 20);
+printf("Content of second string is a max: %s\n", str2);
+memset(str2, 0, sizeof(str2));
+strncpy(str2, str1, 9);
+printf("Content of second string is a part: %s\n", str2);
+memset(str2, 0, sizeof(str2));
+
+strcat(str1, str3);
+printf("Concat string is: %s\n", str1);
+strncat(str3, str1, 11);
+printf("Part Concat is: %s\n", str3);
 
   return 0;
 }
