633c9bee05dc7aa7b2faafd5567233c7163faff4
codemenivo <codemen@abv.bg>
2022-09-01 21:50:49 +0300

    Add files via upload

--- HW_5/Task_1_02_Min.c
+++ HW_5/Task_1_02_Min.c
@@ -1,6 +1,6 @@
 
 #include<stdio.h>
-#define MAX(a, b, c) \
+#define MIN(a, b, c) \
 ((a < b && a < c) ? a : (b < a && b < c) ? b : c)   
     
 
@@ -11,7 +11,7 @@ int main()
   int num2 = 38;
   int num3 = 16;
 
-printf("Max num is: %d\n", MAX(num1, num2, num3));
+printf("Min num is: %d\n", MAX(num1, num2, num3));
 
 
   return 0;
