f4fbb2039014c4e91a26b258c779d89b84e567f5
codemenivo <codemen@abv.bg>
2022-09-01 21:46:30 +0300

    Add files via upload

---
+++ HW_5/Task_1_02_Min.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+#define MAX(a, b, c) \
+((a < b && a < c) ? a : (b < a && b < c) ? b : c)   
+    
+
+
+int main()
+{
+  int num1 = 20;
+  int num2 = 38;
+  int num3 = 16;
+
+printf("Max num is: %d\n", MAX(num1, num2, num3));
+
+
+  return 0;
+}
+
+
+
