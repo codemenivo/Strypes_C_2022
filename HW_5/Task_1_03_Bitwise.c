92b2b4f0ac3a347fdea629f9f729cbca8b8d3b7f
codemenivo <codemen@abv.bg>
2022-09-02 16:02:18 +0300

    Add files via upload

--- HW_5/Task_1_03_Bitwise.c
+++ HW_5/Task_1_03_Bitwise.c
@@ -1,14 +1,24 @@
 #include<stdio.h>
 
 #define SETBIT(mask, bit) (mask |= (1ull << (bit)))
+#define CLEARBIT(mask, bit) (mask &= ~(1ull << (bit)))
+#define INVERSEBIT(mask, bit) (mask ^= (1ull << (bit)))
+#define CHECKBIT(mask, bit) !!(mask & (1ull << (bit)))
 
 
 int main()
 {
     int x = 10; //00001010
+    int y = 15; //00001111
 
 
 
-    printf("Set bit with index 2: %d = %b\n", SETBIT(x, 2), SETBIT(x, 2));
+    printf("Set bit with index 2: %d = %b\n", SETBIT(x, 2), x);
+    
+    printf("Clear bit with index 2: %d = %b\n", CLEARBIT(x, 2), x);
+    
+    printf("Invers bit with index 2: %d = %b\n", INVERSEBIT(y, 2), y);
+    
+    printf("Check bit with index 2: %d = %b\n", CHECKBIT(y, 2), y);
     return 0;
 }
