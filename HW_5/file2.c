3c9d8d6be183093e534812334625c722722fa8fc
codemenivo <codemen@abv.bg>
2022-09-02 21:13:58 +0300

    Add files via upload

--- HW_5/file2.c
+++ HW_5/file2.c
@@ -4,3 +4,12 @@ void swap(int* num1, int* num2)
     *num2 ^= *num1;
     *num1 ^= *num2;
 }
+
+/*
+
+
+ivo@ivo-ASUS-TUF-Gaming-F17-FX706HCB-FX706HCB:~/HomeWorks/HW_5$ gcc -E file2.c -o file2.i
+ivo@ivo-ASUS-TUF-Gaming-F17-FX706HCB-FX706HCB:~/HomeWorks/HW_5$ gcc -S file1.i -o file2.s
+ivo@ivo-ASUS-TUF-Gaming-F17-FX706HCB-FX706HCB:~/HomeWorks/HW_5$ gcc -c file2.s -o file2.o
+
+*/
