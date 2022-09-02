d7b3a3c77105b073dd2e697306382cbcc7b2151b
codemenivo <codemen@abv.bg>
2022-09-02 20:54:58 +0300

    Add files via upload

--- HW_5/file1.c
+++ HW_5/file1.c
@@ -23,4 +23,8 @@ Enter second number: 12
 Before swap: Num1 = 30, Num2 = 12
 After swap: Num1 = 12, Num2 = 30
 
+ivo@ivo-ASUS-TUF-Gaming-F17-FX706HCB-FX706HCB:~/HomeWorks/HW_5$ gcc -E file1.c -o file1.i
+ivo@ivo-ASUS-TUF-Gaming-F17-FX706HCB-FX706HCB:~/HomeWorks/HW_5$ gcc -S file1.i -o file1.s
+ivo@ivo-ASUS-TUF-Gaming-F17-FX706HCB-FX706HCB:~/HomeWorks/HW_5$ gcc -c file1.s -o file1.o
+
 */
---
+++ HW_5/file1.s
@@ -0,0 +1,141 @@
+	.file	"file1.c"
+	.text
+	.globl	swap
+	.type	swap, @function
+swap:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -8(%rbp)
+	movq	%rsi, -16(%rbp)
+	movq	-8(%rbp), %rax
+	movl	(%rax), %edx
+	movq	-16(%rbp), %rax
+	movl	(%rax), %eax
+	xorl	%eax, %edx
+	movq	-8(%rbp), %rax
+	movl	%edx, (%rax)
+	movq	-16(%rbp), %rax
+	movl	(%rax), %edx
+	movq	-8(%rbp), %rax
+	movl	(%rax), %eax
+	xorl	%eax, %edx
+	movq	-16(%rbp), %rax
+	movl	%edx, (%rax)
+	movq	-8(%rbp), %rax
+	movl	(%rax), %edx
+	movq	-16(%rbp), %rax
+	movl	(%rax), %eax
+	xorl	%eax, %edx
+	movq	-8(%rbp), %rax
+	movl	%edx, (%rax)
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	swap, .-swap
+	.section	.rodata
+.LC0:
+	.string	"Enter first number: "
+.LC1:
+	.string	"%d"
+.LC2:
+	.string	"Enter second number: "
+	.align 8
+.LC3:
+	.string	"Before swap: Num1 = %d, Num2 = %d\n"
+	.align 8
+.LC4:
+	.string	"After swap: Num1 = %d, Num2 = %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-16(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	leaq	.LC2(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-12(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	movl	-12(%rbp), %edx
+	movl	-16(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC3(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-12(%rbp), %rdx
+	leaq	-16(%rbp), %rax
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	swap
+	movl	-12(%rbp), %edx
+	movl	-16(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC4(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	subq	%fs:40, %rdx
+	je	.L4
+	call	__stack_chk_fail@PLT
+.L4:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 11.2.0-19ubuntu1) 11.2.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
