449364a732205b04987da4bff89d3b787eed849a
codemenivo <codemen@abv.bg>
2022-09-02 17:05:14 +0300

    Add files via upload

--- HW_5/Task_1_03_Bitwise.c
+++ HW_5/Task_1_03_Bitwise.c
@@ -4,12 +4,15 @@
 #define CLEARBIT(mask, bit) (mask &= ~(1ull << (bit)))
 #define INVERSEBIT(mask, bit) (mask ^= (1ull << (bit)))
 #define CHECKBIT(mask, bit) !!(mask & (1ull << (bit)))
-
+#define SWAP(a,b) (a ^= b ^= a ^= b)
+                    
 
 int main()
 {
     int x = 10; //00001010
     int y = 15; //00001111
+    int k = 9;
+    int m = 14;
 
 
 
@@ -20,5 +23,8 @@ int main()
     printf("Invers bit with index 2: %d = %b\n", INVERSEBIT(y, 2), y);
     
     printf("Check bit with index 2: %d = %b\n", CHECKBIT(y, 2), y);
+
+    SWAP(k, m);
+    printf("Swap two numbers k = 9 and m = 14 => k = %d and m = %d\n", k, m);
     return 0;
 }
